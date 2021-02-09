2bd86ea2144280d3a0e7df369867f14d3be48e82
waiscodes <b.wais@outlook.com>
2021-02-08 21:06:56 -0700

    Added login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <>
+      <div className='login'></div>
+      <div className='register'></div>
+    </>
+  );
+};
+
+export default Login;
